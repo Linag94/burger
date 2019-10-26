1ad5f39a34fb8a5c67995302976537eb1f39d5d0
Linag94 <52180242+Linag94@users.noreply.github.com>
2019-10-26 17:47:43 -0400

    Created the outer for the app, and export the outer at the end of your file.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,44 @@
+// importing for file
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger')
+
+//Setup Routes
+
+// Index Redirect
+router.get('/', function (req, res) 
+{
+  res.redirect('/index');
+});
+
+// Index Page 
+router.get('/index', function (req, res) 
+{
+  burger.selectAll(function(data) 
+  {
+    var burgerObject = { burgers: data };
+    //console.log(burgerObject);
+    res.render('index', burgerObject);
+  });
+});
+
+// Create a New Burger
+router.post('/burger/create', function (req, res) 
+{
+  burger.insertOne(req.body.burger_name, function() 
+  {
+    res.redirect('/index');
+  });
+});
+
+// Devour a Burger
+router.post('/burger/eat/:id', function (req, res) 
+{
+  burger.updateOne(req.params.id, function() 
+  {
+    res.redirect('/index');
+  });
+});
+
+// Export routes
+module.exports = router;
